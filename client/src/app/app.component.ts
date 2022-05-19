5c49bc9c47a4c6966bf630d485411e79d960fd10
kapepa <kapepayx@gmail.com>
2022-05-19 21:18:01 +0300

    19.05

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation} from '@angular/core';
 
-interface IPost {
+export interface IPost {
   id: string,
   title: string,
   text: string,
@@ -9,12 +9,22 @@ interface IPost {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  post: IPost[] = [
+export class AppComponent implements OnInit {
+  posts: IPost[] = [
     { id: 'a1', title: 'First Article', text: 'First Lorem ipsum dolor sit amet'},
     { id: 'b2', title: 'Two Article', text: 'Two Lorem ipsum dolor sit amet'},
     { id: 'c3', title: 'Three Article', text: 'Three Lorem ipsum dolor sit amet'},
   ]
+
+  ngOnInit() {}
+
+  appendPost(post: IPost) {
+    this.posts.unshift(post)
+  }
+
+  delPost(i: number) {
+    this.posts.splice(i,1);
+  }
 }
--- client/src/app/list/list.component.ts
+++ client/src/app/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {IPost} from "../app.component";
 
 @Component({
   selector: 'app-list',
@@ -6,14 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
+  @Input() post: IPost = {} as IPost;
+  @Input() i: number = 0;
+  @Output() delPost = new EventEmitter<number>()
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  delCard() {
-    console.log('del')
+  deletePost(i: number) {
+    this.delPost.emit(i);
   }
-
 }
