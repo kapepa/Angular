5818cfa4e121b9f52510d928db3913afea52963b
kapepa <kapepayx@gmail.com>
2022-05-21 23:41:53 +0300

    21.05

---
+++ client/src/app/counter/counter.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CounterService {
+  count = 0;
+  constructor() { }
+
+  decrease () { this.count -- }
+  increase () { this.count ++ }
+}
