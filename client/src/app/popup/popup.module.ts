7aacfe02e911f5ad18fcdf4f80d7cfe597aa795a
kapepa <kapepayx@gmail.com>
2022-05-31 18:44:03 +0300

    31.05

--- client/src/app/popup/popup.module.ts
+++ client/src/app/popup/popup.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PopupComponent } from './popup.component';
 
-
-
 @NgModule({
   declarations: [
     PopupComponent
